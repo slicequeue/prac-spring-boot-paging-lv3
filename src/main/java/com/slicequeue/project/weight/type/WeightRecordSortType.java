d5c7279fb0aa4da6e41e7ede72a499f64ad38a06
slicequeue <slicequeue@gmail.com>
2023-12-30 20:34:08 +0900

    feat: 열거형 작업 - Order 객체의 경우 변환 시켜주는 convert 정적 메서드 구현

--- src/main/java/com/slicequeue/project/weight/type/WeightRecordSortType.java
+++ src/main/java/com/slicequeue/project/weight/type/WeightRecordSortType.java
@@ -6,6 +6,9 @@ import com.querydsl.core.types.dsl.Expressions;
 import com.slicequeue.project.weight.entity.QWeightRecord;
 import com.slicequeue.project.weight.entity.WeightRecord;
 import lombok.Getter;
+import org.springframework.data.domain.Sort;
+
+import java.util.Arrays;
 
 import static com.slicequeue.project.weight.entity.QWeightRecord.weightRecord;
 
@@ -30,5 +33,13 @@ public enum WeightRecordSortType {
         this.sortingColumnPath = sortingColumnPath;
         this.sortingColumnpathExpression = sortingColumnpathExpression;
         this.nullHandling = NullHandling.NullsLast;
+    }
+
+    public static WeightRecordSortType convert(Sort.Order order) {
+        String property = order.getProperty();
+        return Arrays.stream(values())
+                .filter(each -> each.sortingDtoPath.equals(property))
+                .findFirst()
+                .orElse(null);
     }
 }
