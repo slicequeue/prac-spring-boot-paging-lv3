7cb65ec48293e83fff19176e653491a301d440b1
slicequeue <slicequeue@gmail.com>
2023-12-31 00:40:34 +0900

    feat: 리포지토리 작업 - findPageWeightRecordResponses 메서드 구현

--- src/main/java/com/slicequeue/project/weight/dto/WeightRecordResponse.java
+++ src/main/java/com/slicequeue/project/weight/dto/WeightRecordResponse.java
@@ -1,5 +1,9 @@
 package com.slicequeue.project.weight.dto;
 
+import com.slicequeue.project.statistic.entity.WeightStatistic;
+import com.slicequeue.project.user.entity.User;
+import com.slicequeue.project.weight.entity.WeightImageRecord;
+import com.slicequeue.project.weight.entity.WeightRecord;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,9 +25,11 @@ public class WeightRecordResponse {
 
     private Long userId;
 
+    private String userName;
+
     private float totalAvgWeight;
 
-    private float total;
+    private String totalGrade; // TODO case 이용하여 프로젝션 넘기기, 정렬에도 사용
 
     private List<String> imageUrls;
 
@@ -40,5 +46,28 @@ public class WeightRecordResponse {
         private String comments;
 
         private Long userId;
+
+    }
+
+    public WeightRecordResponse(WeightRecord weightRecord, User user, WeightStatistic weightStatistic) {
+        this.recordId = weightRecord.getId();
+        this.weight = weightRecord.getWeight();
+        this.unit = weightRecord.getUnit().getValue();
+        this.memo = weightRecord.getMemo();
+        this.userId = user.getId();
+        this.userName = user.getName();
+        this.totalAvgWeight = weightStatistic.getTotalAvgWeight();
+//        this.totalGrade
+        this.imageUrls = weightRecord.getImageRecords().stream()
+                .map(WeightImageRecord::getImageUrl)
+                .toList();
+        this.comments = weightRecord.getComments().stream()
+                .map(each -> CommentSummary.builder()
+                        .userId(each.getUserId())
+                        .comments(each.getComment())
+                        .build())
+                .toList();
+        this.createdAt = weightRecord.getCreatedAt();
+        this.updatedAt = weightRecord.getUpdatedAt();
     }
 }
